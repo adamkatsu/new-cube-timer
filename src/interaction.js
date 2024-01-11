427db75f55c1637bb86816ecbcb0dc583114f8b1
Adam Katsu <adamkatsu369@gmail.com>
2024-01-11 22:01:45 +0700

    set active tab indicator

--- src/interaction.js
+++ src/interaction.js
@@ -9,40 +9,64 @@ const contTable = document.querySelector('#cont-table');
 const contMain = document.querySelector('.side-cont-wrapper');
 const wrapperCont = document.querySelector('.body-main');
 
+
+// Button on Click
+
 btnSession.addEventListener('click', () => {
-    checkCont(contSession, contStats, contTable);
+    checkCont(btnSession, contSession, contStats, contTable);
     setTimeout(() => {
         checkPanel();
     }, '300');
 });
 btnStats.addEventListener('click', () => {
-    checkCont(contStats, contTable, contSession);
+    checkCont(btnStats, contStats, contTable, contSession);
     setTimeout(() => {
         checkPanel();
     }, '300');
 });
 btnTable.addEventListener('click', () => {
-    checkCont(contTable, contSession, contStats);
+    checkCont(btnTable, contTable, contSession, contStats);
     setTimeout(() => {
         checkPanel();
     }, '300');
 });
 
-function checkCont(current, hideOne, hideTwo) {
 
+// Functions
+
+function checkCont(btn, current, hideOne, hideTwo) {
+// main side panel show/hide function
     if(
         hideOne.classList.contains('cont-hide') == false ||
         hideTwo.classList.contains('cont-hide') == false) {
+            // wait for existing panel to finish animation
             setTimeout(() => {
                 current.classList.toggle('cont-hide');
             }, "300");
         } else {
+            // switch panel immediately
             current.classList.toggle('cont-hide');
         }
 
-    hideOne.classList.add('cont-hide');
-    hideTwo.classList.add('cont-hide');
+        // hide other panels
+        hideOne.classList.add('cont-hide');
+        hideTwo.classList.add('cont-hide');
 
+        // set active nav button
+        setTimeout(() => {
+            if(current.classList.contains('cont-hide') == false) {
+                for (const item of document.querySelectorAll('.nav-menu-item')) {
+                    item.classList.remove('nav-current')
+                }
+                btn.classList.add('nav-current');
+            } else {
+                for (const item of document.querySelectorAll('.nav-menu-item')) {
+                    item.classList.remove('nav-current')
+                }
+            }
+        }, '300');
+
+    // Shrink main panel in correlation to checkPanel function
     if(wrapperCont.classList.contains('panel-active') == false) {
         wrapperCont.classList.add('panel-active');
         contMain.classList.remove('cont-hide');
@@ -50,17 +74,17 @@ function checkCont(current, hideOne, hideTwo) {
 }
 
 function checkPanel() {
-
+// Expand main panel if side panel is hidden
     if(
         contSession.classList.contains('cont-hide') &&
         contTable.classList.contains('cont-hide') &&
         contStats.classList.contains('cont-hide')) {
+
             wrapperCont.classList.remove('panel-active');
             contMain.classList.add('cont-hide');
+            
         }
-    
 }
-
 
 
 // Breakpoint Check
